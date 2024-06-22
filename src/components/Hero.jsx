9baf461e9ebf6a5da6babf7be2dc5fca82c8c32f
Nuel-Agyei <nuelagyei992@gmail.com>
2024-06-22 20:04:52 +0000

    gsap animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,18 @@
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
 const Hero = () => {
+    useGSAP(() => {
+        gsap.to('#hero', {
+            opacity:1,
+            delay:1.5
+        })
+    },[])
   return (
     <section className="w-full nav-height bg-black relative">
         <div className="h-5/6 w-full flex-center flex-col">
-            <p className="hero-title">iPhone 15 Pro</p>
+            <p id="hero" className="hero-title">iPhone 15 Pro</p>
+            <div></div>
         </div>
     </section>
   )
