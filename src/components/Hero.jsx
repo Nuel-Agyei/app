e2bf8873feb567824590ea3cca77eed696e4781c
Nuel-Agyei <nuelagyei992@gmail.com>
2024-06-22 22:31:33 +0000

    hero card

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
+import { heroVideo, smallHeroVideo } from "../utils";
 
 const Hero = () => {
     useGSAP(() => {
@@ -12,7 +13,11 @@ const Hero = () => {
     <section className="w-full nav-height bg-black relative">
         <div className="h-5/6 w-full flex-center flex-col">
             <p id="hero" className="hero-title">iPhone 15 Pro</p>
-            <div></div>
+            <div className="md:w-10/12 w-9/12 ">
+                <video>
+                    <source src={videoSrc}/>
+                </video>
+            </div>
         </div>
     </section>
   )
