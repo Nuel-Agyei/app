01b62c3a71bddc2a12a6c0f361e4265bb20aa5fe
Nuel-Agyei <nuelagyei992@gmail.com>
2024-06-22 16:53:26 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Highlights from './components/Highlights'
+
+const App = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <main className='bg-black'>
+      <Navbar/>
+      <Hero/>
+      <Highlights/>
+    </main>
+    </>
+  )
+}
+
+export default App
