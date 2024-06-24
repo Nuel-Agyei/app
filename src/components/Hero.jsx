4bc63059db0e963dda7d69c75a58ec74455416ac
Nuel-Agyei <nuelagyei992@gmail.com>
2024-06-24 22:04:14 +0000

    hero final finish

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,20 +25,26 @@ const Hero = () => {
             opacity:1,
             delay:1.5,
         })
+        gsap.to('#cta', {
+            opacity:1,
+            y:-50,
+            delay:3.5,
+        })
     },[])
   return (
     <section className="w-full nav-height bg-black relative">
         <div className="h-5/6 w-full flex-center flex-col">
         <p id="hero" className="hero-title">iPhone 15 Pro</p>
         <div className="md:w-10/12 w-9/12">
-          <video className="pointer-events-none" autoPlay muted playsInline={true} key={videoSrc}>
+          <video className="pointer-events-none" autoPlay loop muted playsInline={true} key={videoSrc}>
             <source src={videoSrc} type="video/mp4" />
           </video>
         </div>
       </div>
 
-      <div id="cta" className="">
-
+      <div id="cta" className="flex flex-col items-center opacity-0 translate-y-20">
+          <a href="#highlights" className="btn">Buy</a>
+          <p className="font-normal text-xl">From $199/month or $999</p>
       </div>
     </section>
   )
