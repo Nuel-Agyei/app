84a4e24d8c6d7de516ab6f41cabf55fc03ac1d45
Nuel-Agyei <nuelagyei992@gmail.com>
2024-07-01 18:08:16 +0000

    model addition

---
+++ src/components/Model.jsx
@@ -0,0 +1,26 @@
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import React from 'react'
+
+const Model = () => {
+
+    useGSAP(() => {
+        gsap.to('#heading', {
+             y:0, 
+             opacity: 1,
+        })
+    }, [])
+  return (
+    <section className='common-padding'>
+        <div className='scrim-max-width'>
+            <h1 id='heading' className='section-heading'>Take a closer look.</h1>
+
+            <div className='flex flex-col items-center mt-5'>
+                <div className='w-full'></div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Model
