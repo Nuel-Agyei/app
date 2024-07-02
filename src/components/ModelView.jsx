a6a783250a5dc6f1e112a99c0a38945879768913
Nuel-Agyei <nuelagyei992@gmail.com>
2024-07-02 12:15:02 +0000

    Lights and suspense

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,5 +1,6 @@
 import { PerspectiveCamera, View } from '@react-three/drei'
-import React from 'react'
+import React, { Suspense } from 'react'
+import Lights from './Lights'
 
 const ModelView = ({index, groupRef, gsapType, controlRef, setRotationSize, size, item}) => {
   return (
@@ -8,7 +9,11 @@ const ModelView = ({index, groupRef, gsapType, controlRef, setRotationSize, size
         className={`border-2  border-red-500 w-full h-full ${index === 2} ? 'right-[-100%] : ''`}>
             <ambientLight intensity={0.3} />
 
-            <PerspectiveCamera makeDefault position={[0, 0, ]}/>
+            <PerspectiveCamera makeDefault position={[0, 0, 4]}/>
+
+            <Lights />
+            <Suspense fallback={<div>Loading</div>}>
+            </Suspense>
         </View>
   )
 }
