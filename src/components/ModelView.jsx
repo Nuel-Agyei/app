7fd3e7d279ebd9570c2bda8bfc1c8841e72fbe4e
Nuel-Agyei <nuelagyei992@gmail.com>
2024-07-02 12:08:53 +0000

    viewport rendering

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 
 const ModelView = ({index, groupRef, gsapType, controlRef, setRotationSize, size, item}) => {
   return (
-        <View></View>
+        <View index={index}
+        id={gsapType}
+        className={`border-2  border-red-500 w-full h-full ${index === 2} ? 'right-[-100%] : ''`}>
+            
+        </View>
   )
 }
 
