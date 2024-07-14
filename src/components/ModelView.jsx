422d53037d00ea753a4d2d6415d622f3add2f8ff
Nuel-Agyei <nuelagyei992@gmail.com>
2024-07-14 17:35:22 +0000

    sizes

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,55 +1,45 @@
+import { Html, OrbitControls, PerspectiveCamera, View } from "@react-three/drei"
+
 import * as THREE from 'three'
-import { OrbitControls, PerspectiveCamera, useTexture, View } from '@react-three/drei'
-import React, { Suspense } from 'react'
-import Lights from './Lights'
-import Loader from './Loader'
-import IPhone from './IPhone'
-import { useGSAP } from '@gsap/react'
-import gsap from 'gsap'
-import * as THREE from 'three' 
+import Lights from './Lights';
+import Loader from './Loader';
+import IPhone from './IPhone';
+import { Suspense } from "react";
 
-const ModelView = ({index, groupRef, gsapType, controlRef, setRotationState, size, item}) => {
-  // useGSAP(()=>{
-  //   gsap.to('#viewbox', {
-  //     ease: 'bounce.in',
-  //     borderRadius: '100%',
-  //     yoyo: true,
-  //   })
-  // }, [])
-  
+const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationState, size, item }) => {
   return (
-    <div>
-      <View index={index}
-        id={gsapType}
-        className={`w-full h-full ${index === 2} ? 'right-[-100%] : ''`}>
-            <ambientLight intensity={0.3} />
+    <View
+      index={index}
+      id={gsapType}
+      className={`w-full h-full absolute ${index === 2 ? 'right-[-100%]' : ''}`}
+    >
+      {/* Ambient Light */}
+      <ambientLight intensity={0.3} />
 
-            <PerspectiveCamera makeDefault position={[0, 0, 4]}/>
+      <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+      <Lights />
 
-            <Lights />
-            <OrbitControls
-              makeDefault
-              ref={controlRef}
-              enableZoom={false}
-              enablePan={false}
-              rotateSpeed={0.4}
-              target={new THREE.Vector3(0, 0, 0)}
-              onEnd={()=>setRotationState(controlRef.current.getAzimuthalAngle()) }
-            />
+      <OrbitControls 
+        makeDefault
+        ref={controlRef}
+        enableZoom={false}
+        enablePan={false}
+        rotateSpeed={0.4}
+        target={new THREE.Vector3(0, 0 ,0)}
+        onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())}
+      /> 
 
-            <group ref={groupRef} name={`${index === 1} ? 'small' : 'large'`} position={[0, 0, 0]}>
-            <Suspense fallback={<Loader/>}>
-            {/* <IPhone 
+      <group ref={groupRef} name={`${index === 1} ? 'small' : 'large`} position={[0, 0 ,0]}>
+        <Suspense fallback={<Loader />}>
+          <IPhone 
             scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
             item={item}
             size={size}
-              /> */}
-            </Suspense>
-            </group>
-            
-        </View>
-    </div>
-        
+          />
+          <Lights />
+        </Suspense>
+      </group>
+    </View>
   )
 }
 
--- src/utils/animations.js
+++ src/utils/animations.js
@@ -1,3 +1,23 @@
-export const animateWithGsapTimeline = (timeline, rotationRef, rotationState) => {
+export const animateWithGsapTimeline = (timeline, rotationRef, rotationState, 
+    firstTarget, secondTarget, animationProps) => {
+        timeline.to(rotationRef.current.rotation, {
+            y: rotationState,
+            duration: 1,
+            ease: 'power2.inOut'
+        })
+        timeline.to(
+            firstTarget, {
+                ...animationProps,
+                ease: 'power2.inOut'
+            },
+            '<'
+        )
 
+        timeline.to(
+            secondTarget, {
+                ...animationProps,
+                ease: 'power2.inOut'
+            },
+            '<'
+        )
 }
