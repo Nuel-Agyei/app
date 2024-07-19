b40867448b2a566d590d53fb933316b9aeffc0f3
Nuel-Agyei <nuelagyei992@gmail.com>
2024-07-19 15:06:11 +0000

    features

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Features = () => {
   return (
-    <div>Features</div>
+    <secction className="h-full common-padding bg-zinc relative overflow-hidden ">
+      <div className='scrim-max-width'>
+        <div className='mb-12 w-full'>
+          <h1 id='features_title' className='section-heading'>Explore the full Story.</h1>
+        </div>
+      </div>
+    </secction>
   )
 }
 
